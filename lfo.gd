2e0dc12141800e5d6b1d5d66eff26565f3a7adab
NotJamGames <77352023+NotJamGames@users.noreply.github.com>
2024-05-18 14:49:27 +0100

    LFO cycles and resets

--- lfo.gd
+++ lfo.gd
@@ -8,9 +8,36 @@ var wave_funcs : Array[Callable] = [calculate_saw, calculate_square, calculate_t
 var curr_wave_func : Callable
 
 
+## The rate of the LFO, in HZ. [br][br]
+## Must be greater than 0.
+@export var rate : float = 60.0 : set = set_rate
+@onready var cycle_duration : float = 1.0 / rate
+var time : float = .0
+
+
+## Sets the strength of the LFO. [br][br] 
+## Accepts negative values. 
+@export var amplitude : float
+
+
+func _process(delta : float) -> void:
+	time += delta
+	if time > cycle_duration:
+		time = fmod(time, cycle_duration) 
+
+
 func set_waveform(new_waveform : Waveforms) -> void:
 	waveform = new_waveform
 	curr_wave_func = wave_funcs[new_waveform]
+
+
+func set_rate(new_rate : float) -> void:
+	if new_rate <=. 0:
+		push_error("Rate must be greater than zero) 
+		return
+
+	rate = new_rate
+	cycle_duration = 1.0 / rate
 
 
 func calculate_saw() -> float:
